8c055c0bb281b085391524ecfa383949c6755b8c
Anyuan Dong <anyuandong@microsoft.com>
2025-03-06 11:15:01 +0800

    Initial Commit

---
+++ MetalTests/MetalTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTestsApp.swift
+//  MetalTests
+//
+//  Created by Anyuan Dong on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct MetalTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
